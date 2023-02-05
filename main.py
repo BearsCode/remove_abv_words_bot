6d5e2dac4deef96a7f351d51ed59144a0729c5d6
Bear'sCode <93822032+BearsCode@users.noreply.github.com>
2023-02-05 21:32:51 +0300

    remove_abv_words

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-def remove_abv_words(text):
-    words = text.split()
-    new_words = [word for word in words if "абв" not in word]
-    return " ".join(new_words)
+import re
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
 
-text = input("Введите текст: ")
-print("Результат:", remove_abv_words(text))
+def remove_abv_words(text):
+    return ' '.join(re.sub(r'\b\w*абв\w*\b', '', word) for word in text.split())
+
+def text_message(update, context):
+    text = update.message.text
+    filtered_text = remove_words(text)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=filtered_text)
+
+def main():
+    bot = telegram.Bot(token='TOKEN')
+    updater = Updater(bot.token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    text_handler = MessageHandler(Filters.text, handle_text_message)
+    dispatcher.add_handler(text_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
